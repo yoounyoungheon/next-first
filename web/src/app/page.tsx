8fe1f606cf2042eae7900d08b76d7ae8f11aa70e
yoounyoungheon <107925716+yoounyoungheon@users.noreply.github.com>
2024-08-23 08:53:59 +0900

    dash-board

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import PuzuLogo from "./ui/puzu-logo";
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Home() {
   return (
-  <>
-    <h2><strong>Welcome</strong></h2>
-  </>
+  <main className="flex min-h-screen flex-col p-6">
+    <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <PuzuLogo />
+    </div> <br/>
+    <p className={`${lusitana.className} text-2xl text-gray-800 md:text-3xl md:leading-normal`}>
+      <strong>Welcome to <a href ="/" className="text-blue-500">Puzu</a> </strong>
+    </p>
+  </main>
   );
 }
--- web/src/app/ui/puzu-logo.tsx
+++ web/src/app/ui/puzu-logo.tsx
@@ -1,13 +1,13 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from './fonts';
 
-export default function AcmeLogo() {
+export default function PuzuLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <p className="text-[44px]">Puzu</p>
     </div>
   );
 }
