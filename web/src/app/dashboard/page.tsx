379b5a7227924cc85e8aeea558c40bfa5d0bc1e8
yoounyoungheon <107925716+yoounyoungheon@users.noreply.github.com>
2024-08-26 16:16:45 +0900

    add dashboard-card

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -1,3 +1,29 @@
+import { Card } from "../ui/dashboard/cards";
+import { lusitana } from "../ui/fonts";
+
 export default function Page(){
-    return <p> This is Dashboard Page ! </p>
+  // To-Do: mock-data
+  const mockData = {
+    numberOfInvoices: 1000,
+    numberOfCustomers: 1000,
+    totalPaidInvoices: 1000,
+    totalPendingInvoices: 1000,
+    revenue: 1000
+  };
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`} >
+        DashBoard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={mockData.totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={mockData.totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={mockData.numberOfInvoices} type="invoices" />
+        <Card title="Total Customers" value={mockData.numberOfCustomers} type="customers" />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        customer
+      </div>
+    </main>
+  );
 }
